5288fb7751c3ea5664c9ce871bcef85c5ea9cedc
Trevor <tlist403@gmail.com>
2021-12-19 20:07:10 -0300

    Started HTML Doc info

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// ReactDOM.render(JSX, document.getElementById('root)) <-- This is how we can use JSX and React.
+
 // We need to loop through the querySelector in order to actually use it.
 (function () {
 const para = document.querySelectorAll('p');
